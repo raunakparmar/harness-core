ace018ca843ddaf04189fc68eea327462c94e732
Arpit Tiwari <arpit.tiwari@harness.io>
2022-01-28 14:58:19 +0530

    [DEL-3180]: compilation error

--- 400-rest/src/test/java/io/harness/service/DelegateNgTokenServiceTest.java
+++ 400-rest/src/test/java/io/harness/service/DelegateNgTokenServiceTest.java
@@ -12,7 +12,6 @@ import static io.harness.rule.OwnerRule.VLAD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-import io.harness.DelegateServiceTestBase;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
@@ -25,14 +24,19 @@ import io.harness.persistence.HPersistence;
 import io.harness.rule.Owner;
 import io.harness.service.impl.DelegateNgTokenServiceImpl;
 
+import software.wings.WingsBaseTest;
+
 import com.google.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
 
 @OwnedBy(HarnessTeam.DEL)
-public class DelegateNgTokenServiceTest extends DelegateServiceTestBase {
+@RunWith(MockitoJUnitRunner.class)
+public class DelegateNgTokenServiceTest extends WingsBaseTest {
   private static final String TEST_ACCOUNT_ID = "testAccountId";
 
   @Inject private HPersistence persistence;
